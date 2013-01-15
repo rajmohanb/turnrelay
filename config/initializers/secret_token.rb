6e6c36a01610608aa1876017f45e7cb48ec0f656
Rajmohan <rajmohan@mindbricks.com>
2013-01-15 23:36:50 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Turnrelay::Application.config.secret_token = '1f22b0d356427132ae5071a0bb8a8aeba4acb811892bd8650434f4bb98406941815403445726d7c74e1658d0b9b02a3ca4773c7c3f203aa8f3111db3db1b1514'
