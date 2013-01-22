fbd3d8b5ea241dbb44cef72ec3b47f36c9cd7664
Rajmohan <rajmohan@mindbricks.com>
2013-01-22 15:03:54 +0530

    Added site controller

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,13 @@
+class SiteController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+
+  def features
+  end
+
+  def pricing
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Turnrelay::Application.routes.draw do
+  get "site/home"
+
+  get "site/help"
+
+  get "site/features"
+
+  get "site/pricing"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
